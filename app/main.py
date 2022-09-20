713999833e393e93fd7136a0184b99e69dfa7c59
MoonChaewoo <103232857+MoonChaeWoo@users.noreply.github.com>
2022-09-21 01:04:53 +0900

    프론트엔드 꾸미기

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ import uvicorn
 from fastapi.templating import Jinja2Templates
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 def create_app():
     # 앱 생성
@@ -28,6 +29,7 @@ def create_app():
 app = create_app()
 config = conf()
 templates = Jinja2Templates(directory=config.TEMPLATES)
+app.mount("/static", StaticFiles(directory=config.STATIC), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 # request : Request를 받아야 html 호출 가능
@@ -36,7 +38,7 @@ def index(request : Request):
         'request' : request,
         'value_1' : 'request_testValue'
     }
-    return templates.TemplateResponse('base.html', context)
+    return templates.TemplateResponse('main.html', context)
 
 if __name__ =="__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
